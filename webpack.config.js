f834485e67880bf887a4bb0549788ac5ca56289d
Keval Indrodia <pattanijeet@gmail.com>
2024-08-16 00:46:28 +0530

    update hero img

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const path = require('path');
 
 module.exports = {
@@ -29,3 +30,36 @@ module.exports = {
     ],
   },
 };
+=======
+const path = require('path');
+
+module.exports = {
+  entry: {
+    bundle1: './js/sphere.js',
+    bundle2: [
+      './js/welcome.anime.min.js',
+      './js/switchTabs.js',
+      './js/nav.min.js',
+      './js/pageAnimations.min.js',
+    ],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
+>>>>>>> Stashed changes
